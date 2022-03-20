cca3c9dd937f03f16fa17702e6cec30ed0feb631
YusufOnatYilmaz <76898984+YusufOnatYilmaz@users.noreply.github.com>
2022-03-20 20:07:52 +0300

    Add files via upload

--- v2.py
+++ v2.py
@@ -1,6 +1,5 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
-import time
 import pyfirmata
 
 board = pyfirmata.Arduino("COM3")
@@ -10,17 +9,14 @@ pin6 = board.get_pin('d:6:s')
 pin9 = board.get_pin('d:9:s')
 pin10 = board.get_pin('d:10:s')
 pin11 = board.get_pin('d:11:s')
+pin_array = [pin6,pin9,pin10,pin11]
+
+
+#### Bunun altı Pyqt5
 
 class Ui_MainWindow(object):
 
     def setupUi(self, MainWindow):
-        self.value1 = 0
-        self.value2 = 0
-        self.value3 = 0
-        self.value4 = 0
-        self.position_1 = [90,90,90,90]
-        self.position_2 = [90,90,90,90]
-        self.go_there = True
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(822, 558)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -78,133 +74,123 @@ class Ui_MainWindow(object):
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
-        self.servo_1.valueChanged.connect(self.pos)
-        self.servo_2.valueChanged.connect(self.pos)
-        self.servo_3.valueChanged.connect(self.pos)
-        self.servo_4.valueChanged.connect(self.pos)
+        self.initiator()
 
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        
+
+        ##### Bunun üstü Pyqt5
+
+    def initiator(self):
+        self.value1 = 0
+        self.value2 = 0
+        self.value3 = 0
+        self.value4 = 0
+        self.position_1 = [90,90,90,90]
+        self.position_2 = [90,90,90,90]
+        self.go_there = True
+
+        #sliderların sınırlarını 100'den 180'e alma
         self.servo_1.setMaximum(180)
         self.servo_2.setMaximum(180)
         self.servo_3.setMaximum(180)
         self.servo_4.setMaximum(180)
+        self.servo_1.setValue(self.value1)
+        self.servo_2.setValue(self.value2)
+        self.servo_3.setValue(self.value3)
+        self.servo_4.setValue(self.value4)
+        pin_array[0].write(self.value1)
+        pin_array[1].write(self.value2)
+        pin_array[2].write(self.value3)
+        pin_array[3].write(self.value4)
 
+        #sliderlerin değerleri değiştiği zaman pos fonksiyonu çalışır.
+        self.servo_1.valueChanged.connect(self.pos)  
+        self.servo_2.valueChanged.connect(self.pos)
+        self.servo_3.valueChanged.connect(self.pos)
+        self.servo_4.valueChanged.connect(self.pos)
+
+        # butonları ilgili fonksiyonlara atama
         self.pushButton.clicked.connect(self.go_pos_1)
         self.pushButton_2.clicked.connect(self.go_pos_2)
         self.pushButton_3.clicked.connect(self.pos_save_1)
         self.pushButton_4.clicked.connect(self.pos_save_2)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
-        
-    def smoother(self):
-        pass
 
-    def servo_writer(self, servo_array):
-        print(servo_array)
-        pin6.write(servo_array[0])
-        pin9.write(servo_array[1])
-        pin10.write(servo_array[2])
-        pin11.write(servo_array[3])
+    # Motorları açı değerlerini birer birer arttırarak kaydedilen pozisyona götürüyoruz.
+    def smoother(self, saved_pos, current_pos):
 
-        '''
-        adder_1 = 0
-        adder_2 = 0
-        adder_3 = 0
-        adder_4 = 0
-
-        i = 0
-        for array in servo_array:
-            if cur_value[i] > array:
-                adder_1 = -1
-            elif cur_value[i] < array:
-                adder_1 = +1
-            if cur_value[i] > array:
-                adder_2 = -1
-            elif cur_value[i] < array:
-                adder_2 = +1
-            if cur_value[i] > array:
-                adder_3 = -1
-            elif cur_value[i] < array:
-                adder_3 = +1
-            if cur_value[i] > array:
-                adder_4 = -1
-            elif cur_value[i] < array:
-                adder_4 = +1
-            i += 1
+        adder = []
+        for i in range(len(saved_pos)):
+            temp = -1 if saved_pos[i] < current_pos[i] else 1 if saved_pos[i] > current_pos[i] else 0
+            adder.append(temp)
 
         while True:
 
-            if servo_array[0] != cur_value[0]:
-                pin6.write(cur_value[0]+adder_1)
-                cur_value[0] += adder_1
+            for i in range(len(saved_pos)):
+                if saved_pos[i] != current_pos[i]:
+                    pin_array[i].write(current_pos[i] + adder[i])
+                    current_pos[i] += adder[i]
             
-            if servo_array[1] != cur_value[1]:
-                pin9.write(cur_value[1]+adder_2)
-                cur_value[1] += adder_2
-
-            if servo_array[2] != cur_value[2]:
-                pin10.write(cur_value[2]+adder_3)
-                cur_value[2] += adder_3
-
-            if servo_array[3] != cur_value[3]:
-                pin11.write(cur_value[3]+adder_4)
-                cur_value[3] += adder_4
-
-            time.sleep(1)
-            if servo_array[0] == cur_value[0] and servo_array[1] != cur_value[1] and servo_array[2] != cur_value[2] and servo_array[3] != cur_value[3]:
-                pin6.write(cur_value[0])
-                pin9.write(cur_value[1])
-                pin10.write(cur_value[2])
-                pin11.write(cur_value[3])
+            board.pass_time(0.01)
+            print(saved_pos, current_pos)
+            if saved_pos[0] == current_pos[0] and saved_pos[1] == current_pos[1] and saved_pos[2] == current_pos[2] and saved_pos[3] == current_pos[3]:
                 break
-        '''
 
     # Sliderlar hareket ettiği zaman bu fonksiyon çalışır.
     def pos(self):
-        #if self.go_there:
-        self.value1 = self.servo_1.sliderPosition()
-        self.value2 = self.servo_2.sliderPosition()
-        self.value3 = self.servo_3.sliderPosition()
-        self.value4 = self.servo_4.sliderPosition()
-        self.value_array = [self.value1,self.value2,self.value3,self.value4]
-        self.servo_writer(self.value_array)
+        if self.go_there:
+            # Sliderların pozisyonunu anlık pozisyonunu alıp motorları çalıştırıyoruz.
+            self.value1 = self.servo_1.sliderPosition()
+            self.value2 = self.servo_2.sliderPosition()
+            self.value3 = self.servo_3.sliderPosition()
+            self.value4 = self.servo_4.sliderPosition()
+            pin6.write(self.value1)
+            pin9.write(self.value2)
+            pin10.write(self.value3)
+            pin11.write(self.value4)
+            self.value_array = [self.value1,self.value2,self.value3,self.value4]
 
-    def go_slider(self,slider_pos):
+
+    def go_slider(self, slider_pos):
         self.servo_1.setValue(slider_pos[0])
-        self.servo_2.setValue(slider_pos[0])
+        self.servo_2.setValue(slider_pos[1])
         self.servo_3.setValue(slider_pos[2])
         self.servo_4.setValue(slider_pos[3])
+
 
     # Save_1'e basıldıktan sonra bu fonksiyon çalışır.
     def pos_save_1(self):
         self.position_1 = [self.value1, self.value2, self.value3, self.value4]
         print(self.position_1)
 
+
     # Save_2'ye basıldıktan sonra bu fonksiyon çalışır.
     def pos_save_2(self):
         self.position_2 = [self.value1, self.value2, self.value3, self.value4]
         print(self.position_2)
 
+
     # 1'e basıldıktan sonra bu fonksiyon çalışır.
     def go_pos_1(self):
-        #self.smoother()
-        self.servo_writer(self.position_1)
-        '''
+        self.smoother(self.position_1,self.value_array)
+        # motorları götürdükten sonra bir de sliderları yeni pozisyona götürüyoruz
         self.go_there = False
         self.go_slider(self.position_1)
         self.go_there = True
-        '''
-    
+
+
     # 2'ye basıldıktan sonra bu fonksiyon çalışır.
     def go_pos_2(self):
-        #self.smoother()
-        self.servo_writer(self.position_2)
-        '''
+        self.smoother(self.position_2,self.value_array)
         self.go_there = False
         self.go_slider(self.position_2)
         self.go_there = True
-        '''
-    
+
+    #### Bunun altı Pyqt5
+
+
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "METU MECH"))
